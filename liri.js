c26d66dcaf6083eb6ac2dc8b3d215740ae081db2
YJ <yair.yoseph@gmail.com>
2018-09-13 21:53:43 -0700

    done

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var dotenv = require("dotenv").config();
 var request = require("request");
 var moment = require("moment");
@@ -7,8 +8,6 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-console.log(Spotify);
-
 var command = process.argv[2];
 
 var input = "";
@@ -17,6 +16,29 @@ for (let i = 3; i < process.argv.length; i++) {
     if (i !== process.argv.length - 1) {
         input += "+";
     }
+}
+
+
+function spotifyInp(songName){
+    spotify.search({ type: 'track', query: songName}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        for (let i = 0; i < data.length; i++) {
+            var string = data[0].JSON.stringify();
+        }
+
+        if (data.tracks.items[0].preview_url === null) {
+            console.log("Sorry no preview link");
+        }
+        else {
+            console.log("Preview link: " + data.tracks.items[0].preview_url);
+        }
+        console.log("Album name: " + data.tracks.items[0].album.name);
+        console.log("Artist name: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song name: " + data.tracks.items[0].name);
+
+    });
 }
 
 
@@ -60,29 +82,36 @@ switch (command) {
                 console.log(movie.Plot);
                 console.log(movie.Actors);
             }
-            // if (input === "") {
-            //     input = 'Mr.Nobody.';
-            // }
-
         });
         break;
 
-    case "do-what-it-says":
+    case "spotify-this-song":
+        if (input === "") {
+
+            input = 'the sign ace of base';
+        }
+        spotifyInp(input);
 
         break;
 
-    case "spotify-this-song":
-        //     var queryUrl = 
-        //     request(queryUrl, function(error, response, body) {
+    case "do-what-it-says":
+        fs.readFile("random.txt", "utf8", function (error, data) {
 
-        //   // If the request is successful (i.e. if the response status code is 200)
-        //   if (!error && response.statusCode === 200) {
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+                return console.log(error);
+            }
 
-        //     // Parse the body of the site and recover just the imdbRating
-        //     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-        //     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-        //   }
-        // });
+            // We will then print the contents of data
+            // console.log(data);
+
+            // Then split it by commas (to make it more readable)
+            var dataArr = data.split(",");
+
+            // We will then re-display the content as an array for later use.
+            var textParse = (JSON.parse(dataArr[1]));
+                spotifyInp(textParse)
+        });
 
         break;
 
